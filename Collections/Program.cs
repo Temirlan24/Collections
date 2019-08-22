4f6ad715c323f3a5b46c94d0ea269d778ee05c36
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-08-23 01:00:19 +0600

    Добавьте файлы проекта.

---
+++ Collections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Задание 1:");
+            ArrayList numberAsArray = new ArrayList { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+            for (int i = 0; i < numberAsArray.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(numberAsArray[i]);
+                }
+            }
+            Console.WriteLine("\n");
+            //Задание 2
+            Console.WriteLine("Задание 2:");
+            List<int> elements = new List<int> { 14, 23, 4, 6, 9, 10, 50 };
+            int arifmetikZnachenie, sum = 0;
+            for (int i = 0; i < elements.Count; i++)
+            {
+                sum += elements[i];
+            }
+            arifmetikZnachenie = sum / elements.Count;
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] > arifmetikZnachenie)
+                {
+                    Console.WriteLine(elements[i]);
+                }
+            }
+        }
+    }
+}
